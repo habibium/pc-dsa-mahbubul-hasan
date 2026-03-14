3294b8ddefa0add524f27e5b8e0843aadd526c90
habibium <habib.prodev@gmail.com>
2026-03-14 17:23:55 +0600

    solve timus-1264

---
+++ src/bin/ch02_timus_1264.rs
@@ -0,0 +1,12 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut input = String::new();
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("Failed to read");
+    let mut input_iter = input.split_whitespace();
+    let n: usize = input_iter.next().unwrap().parse().unwrap();
+    let m: usize = input_iter.next().unwrap().parse().unwrap();
+    println!("{}", n * (m + 1));
+}
