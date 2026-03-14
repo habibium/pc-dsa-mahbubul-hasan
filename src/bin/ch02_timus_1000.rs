7e9a4d0f01b29d487bea5a28c00246bd44183f08
habibium <habib.prodev@gmail.com>
2026-03-14 16:55:53 +0600

    setup project and initial structure

---
+++ src/bin/ch02_timus_1000.rs
@@ -0,0 +1,15 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut input = String::new();
+
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("Failed to read");
+
+    let mut input_iter = input.split_whitespace();
+    let a: i64 = input_iter.next().unwrap().parse().unwrap();
+    let b: i64 = input_iter.next().unwrap().parse().unwrap();
+
+    println!("{}", a + b);
+}
